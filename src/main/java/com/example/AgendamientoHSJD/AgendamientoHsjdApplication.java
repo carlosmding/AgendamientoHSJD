27bee43f33c4f26a7adecb86de1e195f9c0767df
Carlos Pinto <carlos.pinto@udea.edu.co>
2021-09-26 16:51:17 -0500

    Se crea proyecto de agendamiento HSJD en base a proyecto Spring boot para agendamiento

---
+++ src/main/java/com/example/AgendamientoHSJD/AgendamientoHsjdApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgendamientoHSJD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamientoHsjdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamientoHsjdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AgendamientoHSJD/AgendamientoHsjdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgendamientoHSJD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamientoHsjdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
